c19ae6196b102833938591207c5a8bb998096690
Obeid Arash <obeid75arash@gmail.com>
2023-08-19 20:49:27 +0000

    Finishing Week 4, Courser 3

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'home',
     'hello',
     'autos',
+    'cats',
     ]
 
 MIDDLEWARE = [
--- mysite/urls.py
+++ mysite/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('', views.home),
     path('accounts/', include('django.contrib.auth.urls')),
     path('autos/', include('autos.urls')),
+    path('cats/', include('cats.urls')),
+
 ]
