46f8f1c82fe8a9aae6873176ee6d1e345e66cc1e
Obeid Arash <obeid75arash@gmail.com>
2023-08-20 09:30:37 +0000

    Finishin, Course 3, Week 5

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,26 +1,49 @@
 import os
 from django.contrib import admin
-from django.urls import include, path, re_path
+from django.urls import path, include, re_path
+from django.conf import settings
+from django.contrib.auth import views as auth_views
 from django.views.static import serve
-
-from home import views
-
-
-# Up two folders to serve "site" content
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SITE_ROOT = os.path.join(BASE_DIR, 'site')
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('hello', include('hello.urls')),
-    path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls')),
-    re_path(r'^site/(?P<path>.*)$', serve,
-        {'document_root': SITE_ROOT, 'show_indexes': True},
-        name='site_path'
-    ),
-    path('', views.home),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('autos/', include('autos.urls')),
-    path('cats/', include('cats.urls')),
+    path('', include('ads.urls')),  # Change to ads.urls
+    path('admin/', admin.site.urls),  # Keep
+    path('accounts/', include('django.contrib.auth.urls')),  # Keep
+    re_path(r'^oauth/', include('social_django.urls', namespace='social')),  # Keep
 
 ]
+
+# Serve the static HTML
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+urlpatterns += [
+    re_path(r'^site/(?P<path>.*)$', serve,
+        {'document_root': os.path.join(BASE_DIR, 'site'),
+         'show_indexes': True},
+        name='site_path'
+        ),
+]
+
+# Serve the favicon - Keep for later
+urlpatterns += [
+    path('favicon.ico', serve, {
+            'path': 'favicon.ico',
+            'document_root': os.path.join(BASE_DIR, 'home/static'),
+        }
+    ),
+]
+
+# Switch to social login if it is configured - Keep for later
+try:
+    from . import github_settings
+    social_login = 'registration/login_social.html'
+    urlpatterns.insert(0,
+                       path('accounts/login/', auth_views.LoginView.as_view(template_name=social_login))
+                       )
+    print('Using', social_login, 'as the login template')
+except:
+    print('Using registration/login.html as the login template')
+
+# References
+
+# https://docs.djangoproject.com/en/3.0/ref/urls/#include
